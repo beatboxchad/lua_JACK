49bd57f62c42b5dccb82b8281bca31fb817b1515
beatboxchad <chad.cassady@gmail.com>
2013-09-14 18:39:22 -0700

    Added MIDI but the port doesn't work yet, investigating. Also cleaned up the comments and the code

--- lua_jack.c
+++ lua_jack.c
@@ -5,6 +5,8 @@
  * Many thanks to this dude: https://github.com/x42/jack_midi_clock.  And also
  * to the super-badass JACK folks who provided this example:
  * http://jackit.sourceforge.net/cgi-bin/lxr/http/source/example-clients/showtime.c
+ * 
+ * Both of those examples really helped me wrap my mind around everything.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -21,12 +23,6 @@
  */
 
 /*
- * This is some of the first C I've ever written, other than some cannibalized
- * stuff on Arduino. It's gonna be horrible, and probably won't even work for
- * awhile. Please do not read this if bad code makes you cry. Tackling such a
- * complex project as my first go is a decision I alone should have to live
- * with ;)
- * 
  * This is an incomplete implementation, but it works, and that's amazing to
  * me. I will expand this to be a general-purpose interface to JACK from Lua,
  * but I can grab my BBT info now.
@@ -53,22 +49,14 @@
 
 static jack_client_t *client; 	
 static jack_status_t *status;
-
-// My understanding was that each function has its own private stack Lua reads
-// its shit from, each time it's called. Thus Lua has to be the one to remember
-// shit and pass it properly... Like what's our client. How do I share this
-// information? There's no main() here, so how do I handle this in Lua? There
-// is a way, I'm just a noob. James told me that statics will be able to be
-// read, and it does sure enough compile.
-
-// maybe I should have an init function that declares the client pointer
+static jack_port_t *midi_out;
 
 static int showtime (lua_State *L) {
 	static jack_position_t current; 
 	static jack_transport_state_t transport_state;
 	
 
-	transport_state = jack_transport_query(client, &current); // undefined symbol? wtf?
+	transport_state = jack_transport_query(client, &current);
 
 	/* The number of arguments we'll be returning to the stack for Lua to have
 	 * depends on what transport information is currently available, so I'll just
@@ -104,49 +92,75 @@ static int showtime (lua_State *L) {
 		lua_pushnumber(L, current.tick);
 		lua_pushnumber(L, current.beats_per_bar);
 		lua_pushnumber(L, current.beat_type);
-		num_arguments = num_arguments + 5;
+		lua_pushnumber(L, current.ticks_per_beat);
+		lua_pushnumber(L, current.beats_per_minute);
+		num_arguments = num_arguments + 7;
 	}
 	// I don't know what this is for yet, I only really care about BBT actually,
 	// but probably should include it.
-	if (current.valid & JackPositionTimecode) {
+	//if (current.valid & JackPositionTimecode) {
 		lua_pushnumber(L, current.frame_time);
 		lua_pushnumber(L, current.next_time);
-		num_arguments = num_arguments + 2;
-	}
+		lua_pushnumber(L, current.usecs);
+		num_arguments = num_arguments + 3;
+	//}
 	return num_arguments;
 }
 
-/* need to reimplement this, but need to learn how to talk to Lua
-
-void jack_shutdown (void *arg) {
-	exit (1);
-}
-
-*/
 
 
-static int client_init(lua_State *L) {
+static int open_client(lua_State *L) {
 	/* try to become a client of the JACK server */
 	char *client_name = lua_tostring(L, 1);
 	client = jack_client_open(client_name, 0, status);
-		return 1;
-	//put the client reference into the registry
-	//lua_pushstring(L, "client_pointer");
-	//lua_pushlightuserdata(L, *client);
-	//lua_settable(L, LUA_REGISTRYINDEX);
-	// I'm trying to put the the pointer into the registry, and the only way I
-	// can see to do that is pushlightuserdata. Next I need to figure out how to
-	// retrieve it, if I've even done this part right.
+	jack_activate (client); // TODO add some error handling around this. 
+	return 1;
+};
+
+static int close_client(lua_State *L) {
+	jack_client_close (client);
+	return 0;
+};
+
+// register an outbound MIDI port
+static int register_midi_out(lua_State *L) {
+	char *port_name = lua_tostring(L,1);
+	midi_out = jack_port_register(client, port_name, JACK_DEFAULT_MIDI_TYPE, JackPortIsTerminal, 2048);
+	return 0;
+};
+
+// write some data to that port. 
+
+static const struct luaL_Reg lua_jack [] = {
+	{"showtime", showtime},
+	{"open_client", open_client},
+	{"close_client", close_client},
+	{"register_midi_out", register_midi_out},
+	{NULL, NULL}
+};
+
+int luaopen_liblua_jack (lua_State *L) {
+	luaL_newlib(L, lua_jack);
+	return 1;
 }
 
-/* All of the following still needs to be implemented
+/* 
+ * These are leftovers from the example program I adapted this from at first.
+ * Since this is a library I don't know if I need to register a callback for if
+ * jack shuts down.  
+ */
 
+/*
+void jack_shutdown (void *arg) {
+	exit (1);
+}
+*/
 
-	* tell the JACK server to call `jack_shutdown()' if
-		 it ever shuts down, either entirely, or if it
-		 just decides to stop calling us.
-	*
+/* tell the JACK server to call `jack_shutdown()' if it ever shuts down, either
+ * entirely, or if it just decides to stop calling us.  
+ */
 
+/*
 	jack_on_shutdown (client, jack_shutdown, 0);
 
 	 tell the JACK server that we are ready to roll 
@@ -155,23 +169,5 @@ static int client_init(lua_State *L) {
 		fprintf (stderr, "cannot activate client");
 		return 1;
 	}
-	
-int close_client
-	char client_name = lua_tolstring(L, 1, NULL);
-	jack_client_close (client_name);
-	return 0;
 
-/ gotta do that thing with the macro that pushes all the available functions
 */
-
-static const struct luaL_Reg lua_jack [] = {
-	{"showtime", showtime},
-	{"client_init", client_init},
-	{NULL, NULL}
-};
-
-int luaopen_liblua_jack (lua_State *L) {
-	luaL_newlib(L, lua_jack);
-	return 1;
-
-}
--- make_so.sh
+++ make_so.sh
@@ -5,3 +5,4 @@ gcc -g -c -fPIC -Wall lua_jack.c
 gcc -shared -Wl,--export-dynamic,-soname,liblua_jack.so.1 -o liblua_jack.so.1.0 lua_jack.o -ljack
 
 rm lua_jack.o
+cp liblua_jack.so.1.0 ../../lua/lua-midi-sequencing
